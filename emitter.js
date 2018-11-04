44d0366dba38cd41df9fff724bb22d14bfcf0a57
volkov.da <volkov.da@skbkontur.ru>
2018-11-04 10:52:05 +0500

    начал

--- emitter.js
+++ emitter.js
@@ -6,55 +6,110 @@
  */
 const isStar = true;
 
-
-var events = new Map();
-const getEventsForUnsubscribe(event) => {
-    
-}
-
 /**
  * Возвращает новый emitter
  * @returns {Object}
  */
 function getEmitter() {
-    return {
 
+    let events = new Map();
+
+    const getEventsForEmit = event => {
+        var result = [event];
+        var tempEvent = event;
+        var index = tempEvent.lastIndexOf('.');
+        while (index >= 0) {
+            tempEvent = tempEvent.slice(0, index);
+            result.push(tempEvent);
+            index = tempEvent.lastIndexOf('.');
+        }
+
+        return result;
+    };
+
+    const subscribeOnEvent = (event, context, handler, emitCondition = count => count || true) => {
+        if (!events.has(event)) {
+            events.set(event, []);
+        }
+        let eventObj = {
+            context: context,
+            handler: handler,
+            counter: 0,
+            condition: emitCondition
+        };
+        events.get(event).push(eventObj);
+    };
+
+    const getEventsForOff = event => {
+        return Array.from(events.keys())
+            .filter(current => current.startsWith(event) &&
+                (current.charAt(event.length) === '.' || current.length === event.length));
+    };
+
+    return {
 
         /**
          * Подписаться на событие
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {Object} emitter
          */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
-            events.set(event, { context: context, handler: handler });
+            // console.info(event, context, handler);
+            if (this.subscribersCount === undefined) {
+                this.subscribersCount = 0;
+            }
+            subscribeOnEvent(event, context, handler);
+
+            return this;
         },
+
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object} emitter
          */
         off: function (event, context) {
-            console.info(event, context);
-            getEventsForUnsubscribe(event).forEach(item => {
-                if (events.get(item) !== undefined) {
-                    events.delete(item);
+            const contextAreEqual = (one, another) => {
+                return one.focus === another.focus && one.wisdom === another.wisdom;
+            };
+            // console.info(event, context);
+            getEventsForOff(event).forEach(element => {
+                let eventObjArray = events.get(element);
+                if (eventObjArray !== undefined) {
+                    events.set(element, eventObjArray
+                        .filter(current => !contextAreEqual(current.context, context)));
                 }
-            })
+            });
+
+            return this;
         },
+
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object} emitter
          */
         emit: function (event) {
-            console.info(event);
-            getEventsForEmit(event).forEach(item => {
-                const { context, handler } = events(item);
-                handler.call(context);
+            // console.info(event);
+            getEventsForEmit(event).forEach(eventForEmit => {
+                var items = events.get(eventForEmit);
+                if (items !== undefined) {
+                    items.forEach(i => {
+                        const { context, handler, counter, condition } = i;
+                        if (condition(counter)) {
+                            handler.call(context);
+                            i.counter += 1;
+                        }
+                    });
+                }
             });
+
+            return this;
         },
 
         /**
@@ -64,9 +119,13 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} times – сколько раз получить уведомление
+         * @returns {Object} emitter
          */
         several: function (event, context, handler, times) {
             console.info(event, context, handler, times);
+            subscribeOnEvent(event, context, handler, count => count < times);
+
+            return this;
         },
 
         /**
@@ -76,9 +135,13 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} frequency – как часто уведомлять
+         * @returns {Object} emitter
          */
         through: function (event, context, handler, frequency) {
             console.info(event, context, handler, frequency);
+            subscribeOnEvent(event, context, handler, count => count % frequency === 0);
+
+            return this;
         }
     };
 }
